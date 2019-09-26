9c0874374e4789a1ff9f5cd0381353146bcc9e51
emilgamad <emilgamad@gmail.com>
2019-09-26 12:36:55 +0800

    Removed Sync Order

--- SyncOrder.py
+++
@@ -1,8 +0,0 @@
-class SyncOrder():
-    def __init__(origin_store_variant_id,
-                 origin_store_item_levels,
-                 variant_title,
-                 destination_store_variant_id,
-                 destination_store_item_levels):
-
-        self, 
